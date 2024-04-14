95e7b2cb507b2bd84f0349604ef07fcc3e764649
hoprik <42morozov42@gmail.com>
2024-04-14 13:19:15 +0300

    Mysql fix

--- app/api/v1/link/create/route.ts
+++ app/api/v1/link/create/route.ts
@@ -1,15 +1,17 @@
-import mysql from 'serverless-mysql';
+import mysql from 'mysql2/promise';
 import md5 from 'crypto-js/md5';
-const db = mysql({
-    config: {
-        host: process.env.MYSQL_HOST,
-        port: process.env.MYSQL_PORT,
-        database: process.env.MYSQL_DATABASE,
-        user: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASSWORD
-    }
-});
+
 export async function POST(req: Request) {
+    // @ts-ignore
+    let _tmp_webstorm_ = +process.env.MYSQL_PORT;
+    // @ts-ignore
+    const db = mysql.createPool({
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        port: _tmp_webstorm_,
+        database: process.env.MYSQL_DATABASE,
+    });
     const {url} = await req.json()
     if (url == undefined){
         return Response.json({"error": "Missing URL"})
--- app/api/v1/link/get/route.ts
+++ app/api/v1/link/get/route.ts
@@ -1,19 +1,21 @@
-import mysql from 'serverless-mysql';
-const db = mysql({
-    config: {
-        host: process.env.MYSQL_HOST,
-        port: process.env.MYSQL_PORT,
-        database: process.env.MYSQL_DATABASE,
-        user: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASSWORD
-    }
-});
+import mysql from "mysql2/promise";
+
 export async function POST(req: Request) {
+    // @ts-ignore
+    let _tmp_webstorm_ = +process.env.MYSQL_PORT;
+    // @ts-ignore
+    const db = mysql.createPool({
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        port: _tmp_webstorm_,
+        database: process.env.MYSQL_DATABASE,
+    });
     const {code} = await req.json()
     if (code == undefined){
         return Response.json({"error": "Missing code"})
     }
-    const result = await db.query(`select * from shorturl where code = "${code}"`)
+    const [result] = await db.query(`select * from shorturl where code = "${code}"`)
     await db.end()
     try {
         // @ts-ignore
