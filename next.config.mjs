63a221313be62f52d08097da1b257310192585d0
hoprik <42morozov42@gmail.com>
2024-04-23 21:09:34 +0300

    Build exp

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
     async headers() {
         return [
             {
-                source: '/api/*',
+                source: '*',
                 headers: [
                     {
                         key: 'Cache-Control',
