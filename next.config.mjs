24888df0baede0af6313b217f703d1ed923960d0
hoprik <42morozov42@gmail.com>
2024-04-23 21:11:49 +0300

    Build exp2

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,43 @@ const nextConfig = {
     async headers() {
         return [
             {
-                source: '*',
+                source: '/api/v1/favicon',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'no-store, max-age=0',
+                    },
+                ],
+            },
+            {
+                source: '/api/v1/link/create',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'no-store, max-age=0',
+                    },
+                ],
+            },
+            {
+                source: '/api/v1/link/get',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'no-store, max-age=0',
+                    },
+                ],
+            },
+            {
+                source: '/api/v1/link/projects/getProjects',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'no-store, max-age=0',
+                    },
+                ],
+            },
+            {
+                source: '/api/v1/link/timecodding',
                 headers: [
                     {
                         key: 'Cache-Control',
