09c6baada61f35b0ebb9f96dfa78cc53b7af2b20
hoprik <42morozov42@gmail.com>
2024-04-23 21:13:05 +0300

    Build exp3

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
-    ignoreDuringBuilds: true,
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
     async headers() {
         return [
             {
